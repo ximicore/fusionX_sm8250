04bf402ea2b61597e52ec3235d242a3abdd73a79
Andriannn <andrianp920@gmail.com>
2025-11-24 15:10:06 +0000

    Revert "fs/proc/task_mmu.c: added missing structure for hiding"
    This reverts commit 927b8d0f4ae39501cec161c1d1315725927136f7.
    
    Change-Id: I44802a3bd274c45808c41fcbd59e3b3ff46e4813
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -346,23 +346,6 @@ static void show_vma_header_prefix(struct seq_file *m,
 	seq_putc(m, ' ');
 }
 
-static void show_vma_header_prefix_fake(struct seq_file *m,
-	unsigned long start, unsigned long end,
-	vm_flags_t flags, unsigned long long pgoff,
-	dev_t dev, unsigned long ino)
-{
-seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
-seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
-start,
-end,
-flags & VM_READ ? 'r' : '-',
-flags & VM_WRITE ? '-' : '-',
-flags & VM_EXEC ? '-' : '-',
-flags & VM_MAYSHARE ? 's' : 'p',
-pgoff,
-MAJOR(dev), MINOR(dev), ino);
-}
-
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 {
