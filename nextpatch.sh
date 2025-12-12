248da4fe0d660c45ad816484131d9afb99ef8df7
SenseiiX <senseixdev@gmail.com>
2025-12-12 21:13:27 +0800

    Kernel: Switch from KSUN to RKSU
    Change-Id: I5da1749fe5f5293b914310e69f05c9c6d10e85bf
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,4 +1,2 @@
-rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/SenseiiX/KernelSU-Next/next/kernel/setup.sh" | bash -s next
-echo "Adding more manager support..."
-echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
+rm -rf KernelSU
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
